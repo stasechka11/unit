a2b9293ee8ca79d9f01d6fc32b7202abb656a98b
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-02-03 18:09:36 +0400

    added tests for CashBackHacker

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,84 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashBackHackerTest {
+    @Test
+    public void shouldRemain100IfAmount900() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 900;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain0IfAmount1000() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1000;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain0IfAmount3000() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 3000;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain999IfAmount1() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 999;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain500IfAmount500() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 500;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 500;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain900IfAmount1100() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1100;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain1000IfAmount0() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 0;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 1000;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
